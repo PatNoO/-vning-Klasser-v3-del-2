6971a0dacd13e1dddd5f689aa383ffea94582520
PatNoO <Noordh94@hotmail.se>
2025-09-17 11:36:42 +0200

    Added a user and did a test run to see how it works

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,18 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Account account = new Account(1000 , "Patrik");
+
+        System.out.println(account);
+
+        account.deposit(500);
+        System.out.println(account);
+
+        account.withdraw(1000);
+        System.out.println(account);
+
+
+
 
     }
 }
