d060967edcc57282633e39faa07790bf8270d1e0
PatNoO <Noordh94@hotmail.se>
2025-09-17 08:08:11 +0200

    added Class account with constructors and methods to handel balance on a account

--- src/Account.java
+++ src/Account.java
@@ -1,2 +1,29 @@
 public class Account {
+
+    /// 2 stycken instansvariabler "balance" och "name"
+    double balance;
+    String name;
+
+    ///  Konstruktor för att ta in "balance"
+    public Account(double balance) {
+        this.balance = balance;
+    }
+    /// Konstruktor för att sätta "balance" till 0.
+    public Account() {
+        this.balance = 0;
+    }
+
+    /// Metod för att minska saldot ("balance")
+    public double withdraw(double amount) {
+        this.balance -= amount;
+        return balance;
+    }
+
+    /// Metod för att öka saldot ("balance")
+    public double deposit(double amount) {
+        this.balance += amount;
+        return balance;
+    }
+
+    
 }
